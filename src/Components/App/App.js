1acc06260b8eb9ee036ad936f3ac18e54ba1272f
Anshul Dubey <anshuldubey2166@gmail.com>
2019-04-15 18:40:11 +0530

    Routes added

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
-
+import { Switch, Route } from "react-router-dom";
 import Form from "../Form/Form";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Form />
+        <Switch>
+          <Route exact path="/" component={Form} />
+          <Route exact path="/form" component={Form} />
+        </Switch>
       </div>
     );
   }
