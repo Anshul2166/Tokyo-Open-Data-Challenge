f97f4b2d5317177b21a7ab2b526d2864001a3fa8
Anshul Dubey <anshuldubey2166@gmail.com>
2019-05-05 00:05:50 +0530

    Added examples for flask

--- server/flask/index.py
+++ server/flask/index.py
@@ -1,9 +1,31 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
+from routes.users import index_blueprint
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/flask')
+def hello_flask():
+   return 'Hello Flask'
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/python/')
+def hello_python():
+   return 'Hello Python'
+
+@app.route('/admin')
+def hello_admin():
+   return 'Hello Admin'
+
+@app.route('/guest/<guest>')
+def hello_guest(guest):
+   return 'Hello %s as Guest' % guest
+
+@app.route('/user/<name>')
+def hello_user(name):
+   if name =='admin':
+      return redirect(url_for('hello_admin'))
+   else:
+      return redirect(url_for('hello_guest',guest = name))
+
+app.register_blueprint(index_blueprint)
+
+if __name__ == '__main__':
+   app.run()
